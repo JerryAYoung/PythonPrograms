5085532e94826a10364f2770503771b2bd95bc51
Jerry A Young <jerryandrewyoung@gmail.com>
2020-01-19 12:52:46 -0800

    Update algorithmPractice.py

--- algorithmPractice.py
+++ algorithmPractice.py
@@ -1,19 +1,22 @@
 ##Linear Search
-
-array = [3, 7, 9]
-
+unsortedArray = [12, 8, 10, 5, 40, 67]
 def linearSearch(x, array):
 	for i in range(len(array)):
 		if array[i] == x:
 			return i
-		return -1
+	return -1
+
+result = linearSearch(5, unsortedArray)
+print(f"Linear Search of {unsortedArray}")
+print(f"The index position of 5 is: {result}!")
 
 
+##Binary Search
 sortedArray = [1, 3, 4, 6, 7, 8, 10]
-
 def binarySearch(sortedArray, l, r, x):
 	if r >= l:
 		mid = l + (r-1)/2
+		mid = int(mid)
 		if sortedArray[mid] == x:
 			return mid
 		elif sortedArray[mid] > x:
@@ -24,3 +27,5 @@ def binarySearch(sortedArray, l, r, x):
 		return -1
 
 result = binarySearch(sortedArray, 0, len(sortedArray)-1, 7)
+print(f"Binary Search of {sortedArray}")
+print(f"The index position of 7 is: {result}!")
